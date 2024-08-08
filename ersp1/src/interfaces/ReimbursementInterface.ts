77c5730fbaa88f7d18ac0b5521854d1e9d949c1b
Matthew Taillon <mtaillon9033@gmail.com>
2024-08-08 09:18:49 -0400

    Delete ersp1 directory

--- ersp1/src/interfaces/ReimbursementInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface ReimbursementInterface{
-    reimbursementsId?:number,
-    description:string,
-    amount:number
-    status:string
-}
--- ersp1/src/interfaces/RoleInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface RoleInterface{
-    rolesId?:number,
-    name:string,
-    baseSalary:number
-    basePTO:number
-}
